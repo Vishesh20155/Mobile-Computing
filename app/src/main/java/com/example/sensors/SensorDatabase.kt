e7de0b3b735a5f77ba34a1b90d36fb8441786239
Vishesh Rangwani <vishesh20155@iiitd.ac.in>
2023-04-10 22:30:44 +0530

    Changed version of DB

--- app/src/main/java/com/example/sensors/SensorDatabase.kt
+++ app/src/main/java/com/example/sensors/SensorDatabase.kt
@@ -3,7 +3,7 @@ package com.example.sensors
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ProximitySensorData::class, LightSensorData::class, GeomagneticSensorData::class], version = 1)
+@Database(entities = [ProximitySensorData::class, LightSensorData::class, GeomagneticSensorData::class], version = 2)
 abstract class SensorDatabase: RoomDatabase() {
     abstract fun proximityDao(): ProximityDao
     abstract fun lightDao(): LightDao
